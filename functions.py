c79c92049c750abb79f81efcd854766487d6d3ec
maksud <maksud0217@icloud.com>
2026-04-17 06:34:00 +0900

    feat: learn function and its define and  call steps

---
+++ functions.py
@@ -0,0 +1,30 @@
+''' FUNCTIONS
+(1) DEFINE vs CALL
+(2) Parametr vs Argument
+(3) Keyword  & default arguments
+(4) Scope
+'''
+
+print("====== DEFINE (parametr) vs CALL (argument) ======")
+# build in function > print() type()
+# Function - reusable block of code! - Malum bir mantiqni ishga tushurib beradigan code bloq
+# Instead of block {} in JAVA, Python uses indentation!
+
+
+# DEFINE - parametr (build)
+# pass   # pythonda buni qoyish kerak hech narsa mavjud emas hechnarsa qilma degan manoni bildiradi 
+def greet(a):
+   print(f"How do you do, {a}")
+
+
+def greeting(b):
+      print("greeting is executed")
+      return f"Hi {b}"
+
+
+# CALL - argument (execute)
+result1 = greet('Martin')
+print("result1:", result1 )
+
+result2 = greeting("Justin")
+print("result2:", result2)
