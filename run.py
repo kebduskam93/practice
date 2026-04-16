6ccd0937de4fe7df93a174f1e0c9286a049d9b1c
maksud <maksud0217@icloud.com>
2026-04-16 11:55:21 +0900

    feat: python system variable

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ print(message)
 result = type(message)
 print("result:", result)
 
-'''' In Python, there are builtin tools:
+''' In Python, there are builtin tools:
 (1) TYPES > int float str list dict
 (2) FUNCTION > print() len() input() type()
 (3) CONSTANT > True False None
