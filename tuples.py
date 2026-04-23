a3f69abe03d6de555dff9d90e904450d72b8afe2
maksud <maksud0217@icloud.com>
2026-04-23 19:45:41 +0900

    feat: unpacking arguments via dictionary

--- tuples.py
+++ tuples.py
@@ -57,3 +57,17 @@ print("------")
 calculate(0, 2, 300)
 print("----")
 calculate(5, 7)
+
+print("-------------")
+# **kwargs > dictionary
+
+
+def introduce(**kwargs):
+    print(f"the type(**kwargs) value: {type(kwargs)}")
+    print(f"Hi, I am {kwargs["name"]} and I am {kwargs["age"]} year old!")
+    pass
+
+
+# CALL
+introduce(name="Justin", age=25)
+introduce(name="Shawn", age=35)
