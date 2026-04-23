856552de99d0b14673fc6c11ea2dcaa40e4ea539
maksud <maksud0217@icloud.com>
2026-04-23 19:18:17 +0900

    feat: unpacking argument

--- tuples.py
+++ tuples.py
@@ -22,9 +22,38 @@ print("before fruits:", fruits)
 fruits[2] = "melon"
 print("after fruits", fruits)
 
-# tuple (we can not mutate tuple )
+# tuple (we can not mutate tuple ) - Qiymatini o'zgartiraolmaymiz
 animals = ("dog", "cat", "fish", "lion")
 tuple_obj = ("MIT", 100, True, None)
 
 print(animals[0])
-animals[0] = "bird"
+# animals[0] = "bird"
+
+# try avoid this
+people = "Andrew", "John"
+animals = "dog"
+
+print("====== Unpacking arguments =====")
+groups = ["MIT", "FLEXY", "DEVEX", "MG"]
+(x, y, *z) = groups
+print(f"the x: {x} and y: {y}")
+print("z:", z)  # list
+
+
+# *args > tuple
+def calculate(*args):
+    print("*args >", args)
+    total = 1
+    for x in args:
+        total *= x
+    print(f"the type(args) value: {type(args)}")
+    print(f"the total value: {total}")
+    return total
+
+
+    # CALL
+calculate(1, 7, 2, 3)
+print("------")
+calculate(0, 2, 300)
+print("----")
+calculate(5, 7)
