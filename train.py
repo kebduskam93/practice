a05c5a02ccdf3ca82e1193c0b3d1518465f2c383
maksud <maksud0217@icloud.com>
2026-05-01 17:58:15 +0900

    feat: G-TASK (PYTHON)

---
+++ train.py
@@ -0,0 +1,14 @@
+# G-TASK (PYTHON)
+
+# Shunday function tuzingki unga integerlardan iborat array pass 
+# bolsin va function bizga osha arrayning eng katta qiymatiga tegishli 
+# birinchi indexni qaytarsin.
+# MASALAN: get_highest_index([5, 21, 12, 21, 8]) return qiladi 1 sonini.
+
+# Masalani yechimi
+
+def get_highest_index(nums):
+      return nums.index(max(nums))
+
+result = get_highest_index([5, 21, 12, 21, 8])
+print("result:", result) 
